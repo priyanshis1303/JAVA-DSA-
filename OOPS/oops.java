9c4f6a09da8d582999513a184b558a4581607412
priyanshis1303 <priya.singhs1303@gmail.com>
2025-08-12 16:08:08 +0530

    Acess modifiers

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -11,13 +11,23 @@ public class oops {
           p1.settip(5);
           System.out.println(p1.tip);
 
+          //Access modifiers 
+//first make object 
+
+          BankAccount bank = new BankAccount();
+          bank.setusername("priya");
+          System.out.print(bank.username);
+
+          bank.setPassword("85kkhhhhh");
+       //   System.out.println(bank.password);
+       //the above code wot work a ste password is declared 
+       //as private so it is only accessible in the class it is described in hence you 
+       //can set the password but not gt it printed from th main class 
+
      }
-    
-    
-    
-    
-    
-    }
+
+
+     }
 // creating  blueprint of pen (class)
     class Pen{
      String color;
@@ -32,4 +42,23 @@ public class oops {
      }
 
     }
+
+// Access Modifiers 
+class BankAccount{
+ String username;
+ private String password ;
+
+ public void setusername(String usernamenew){
+     username=usernamenew;
+ }
+
+ public void setPassword(String pwd){
+     password=pwd;
+ }
+
+
+
+
+
+}
 
